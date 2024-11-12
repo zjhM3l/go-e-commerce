712cfd617b52f9a54a24ac01a4bdaa399175104d
zjhM3l <zhangjiahe24@outlook.com>
2024-11-12 13:03:29 -0600

    hertz ini

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
 
 func main() {
-  fmt.Println("Chat App v0.01")
+    // server.Default() creates a Hertz with recovery middleware.
+    // If you need a pure hertz, you can use server.New()
+    h := server.Default()
+
+    h.GET("/hello", func(ctx context.Context, c *app.RequestContext) {
+        c.String(consts.StatusOK, "Hello hertz!")
+    })
+
+    h.Spin()
 }
